fa9ed6f18db61fe88c3ded975e684d516c621f42
painchornds <2606249268@qq.com>
2024-05-01 18:18:31 +0800

    first

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  // publicPath: '/'
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : './',
+  configureWebpack: {
+    externals: {
+      echarts: 'echarts',
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      dayjs: 'dayjs',
+      'element-ui': 'ELEMENT',
+      'vue-quill-editor': 'VueQuillEditor',
+      'vuex-persistedstate': 'createPersistedState'
+    }
+  }
+})
